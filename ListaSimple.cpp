dd47cf461e5c62f12d272a437b2d8d8e00d11eb0
Subtedd <subtedd@outlook.com>
2022-12-02 01:20:55 -0600

    agregue la funcion de guardar los datos de la lista

--- ListaSimple.cpp
+++ ListaSimple.cpp
@@ -5,13 +5,27 @@ using namespace std;
 
 class Nodo
 {
+ public:   
   Nodo *siguiente;
   string sitio;
   string usuario;
   string contrasenia;
 };
 
+Nodo *listaContrasenias = NULL;
+
+void guardarDatosLista(Nodo *&, string, string, string);
+
 int main()
 {
 
+}
+
+void guardarDatosLista(Nodo *&listaContrasenias, string SitiO, string UsuariO, string ContraseniA)
+{
+    Nodo *nuevoNodo = new Nodo();
+    nuevoNodo->sitio = SitiO;
+    nuevoNodo->usuario = UsuariO;
+    nuevoNodo->contrasenia = ContraseniA;
+    nuevoNodo->siguiente = NULL;
 }
